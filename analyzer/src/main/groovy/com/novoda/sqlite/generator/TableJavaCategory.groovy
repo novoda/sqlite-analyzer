5854e87086b244738d0457f0ae5fc6172766eee6
Volker Leck <volker.leck@gmail.com>
2014-09-28 19:14:45 +0200

    remove commented unused statement

--- analyzer/src/main/groovy/com/novoda/sqlite/generator/TableJavaCategory.groovy
+++ analyzer/src/main/groovy/com/novoda/sqlite/generator/TableJavaCategory.groovy
@@ -4,8 +4,6 @@ import com.novoda.sqlite.model.Table
 
 class TableJavaCategory {
 
-//    private static orgColumns = Table.metaClass.getMetaMethod('getColumns', [] as Column[])
-
     static Column[] getNoIdColumns(Table table) {
         table.columns.grep({it.name != '_id'})
     }
