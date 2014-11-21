e33cc3825d1c37012b178864f37bf979656b394f
Daniele Bonaldo <dany.bony@gmail.com>
2014-11-21 12:53:41 +0100

    Add comments explaining the use of Class.forName("org.sqlite.JDBC");

--- analyzer/src/main/java/com/novoda/sqlite/MigrationsConnector.java
+++ analyzer/src/main/java/com/novoda/sqlite/MigrationsConnector.java
@@ -21,6 +21,8 @@ public class MigrationsConnector implements Connector {
     @Override
     public Connection connect() throws SQLException {
         try {
+            // Dynamically load the class in order to correctly initialise JDBC drivers, as described in
+            // http://stackoverflow.com/questions/6740601/what-does-class-fornameorg-sqlite-jdbc-do/6740632#6740632
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
