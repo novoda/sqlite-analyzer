5c5d1a09a0137f4b7fc3af3b06e6d924c13f1a92
Volker Leck <volker.leck@gmail.com>
2014-12-21 00:31:09 +0100

    move affinity computation into DataAffinity, more final variables

--- analyzer/src/main/java/com/novoda/sqlite/model/Column.java
+++ analyzer/src/main/java/com/novoda/sqlite/model/Column.java
@@ -5,14 +5,14 @@ import com.novoda.sqlite.StringUtil;
 public final class Column {
     private final String name;
     private final String type;
-    private boolean nullable;
-    private DataAffinity affinity;
+    private final boolean nullable;
+    private final DataAffinity affinity;
 
     public Column(String name, String type, boolean nullable) {
         this.name = name;
         this.type = type;
         this.nullable = nullable;
-        this.affinity = computeAffinity(type);
+        this.affinity = DataAffinity.fromType(type);
     }
 
     public String getName() {
@@ -47,34 +47,5 @@ public final class Column {
         return affinity == DataAffinity.NUMERIC && type.toLowerCase().contains("bool");
     }
 
-    /*
- * See http://www.sqlite.org/datatype3.html
- * section 2.1 Determination of column affinity
- */
-    private DataAffinity computeAffinity(String type) {
-        String deftype = type.toLowerCase();
-        if (deftype.contains("int")) {
-            return DataAffinity.INTEGER;
-        }
-        if (containsOneOf(deftype, "char", "clob", "text")) {
-            return DataAffinity.TEXT;
-        }
-        if (containsOneOf(deftype, "real", "floa", "doub")) {
-            return DataAffinity.REAL;
-        }
-        if (containsOneOf(deftype, "blob") || deftype.equals("")) {
-            return DataAffinity.NONE;
-        }
-        return DataAffinity.NUMERIC;
-    }
-
-    private boolean containsOneOf(String toCheck, String... values) {
-        for (String value : values) {
-            if (toCheck.contains(value)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
 }
--- analyzer/src/main/java/com/novoda/sqlite/model/DataAffinity.java
+++ analyzer/src/main/java/com/novoda/sqlite/model/DataAffinity.java
@@ -2,4 +2,39 @@ package com.novoda.sqlite.model;
 
 public enum DataAffinity {
     TEXT, NUMERIC, INTEGER, REAL, NONE;
+
+    public static DataAffinity fromType(String type) {
+        return computeAffinity(type);
+    }
+
+    /**
+     * See http://www.sqlite.org/datatype3.html
+     * section 2.1 Determination of column affinity
+     */
+    private static DataAffinity computeAffinity(String type) {
+        String deftype = type.toLowerCase();
+        if (deftype.contains("int")) {
+            return DataAffinity.INTEGER;
+        }
+        if (containsOneOf(deftype, "char", "clob", "text")) {
+            return DataAffinity.TEXT;
+        }
+        if (containsOneOf(deftype, "real", "floa", "doub")) {
+            return DataAffinity.REAL;
+        }
+        if (containsOneOf(deftype, "blob") || deftype.equals("")) {
+            return DataAffinity.NONE;
+        }
+        return DataAffinity.NUMERIC;
+    }
+
+    private static boolean containsOneOf(String toCheck, String... values) {
+        for (String value : values) {
+            if (toCheck.contains(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
