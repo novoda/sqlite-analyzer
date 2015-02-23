2376b5aae1bf1c1cac37f982b8cf9a42fcaded06
Friedger Müffke <friedger@gmail.com>
2015-02-23 13:19:55 +0100

    use 4 spaces for indent

--- analyzer/src/main/groovy/com/novoda/sqlite/generator/GetColumnFromCursorGenerator.groovy
+++ analyzer/src/main/groovy/com/novoda/sqlite/generator/GetColumnFromCursorGenerator.groovy
@@ -7,13 +7,13 @@ class GetColumnFromCursorGenerator {
 
     private static final String TEMPLATE = '''\
 public static $returnType $prefix$methodName(android.database.Cursor cursor) {
-  int index = cursor.getColumnIndexOrThrow("$rowName");\
+    int index = cursor.getColumnIndexOrThrow("$rowName");\
 <% if(nullable) {%>
-  if (cursor.isNull(index)) {
-    return null;
-  }\
+    if (cursor.isNull(index)) {
+        return null;
+    }\
 <% } %>
-  return cursor.get$cursorFunction(index)<% if (isBool) out << " > 0" %>;
+    return cursor.get$cursorFunction(index)<% if (isBool) out << " > 0" %>;
 }
 '''
     private final Column column
