c4bd43047c39bc4f2068850b9973bdcde59d9324
Volker Leck <volker.leck@gmail.com>
2015-04-16 12:53:58 +0200

    Add support for library variants

--- analyzer/src/main/groovy/com/novoda/sqlite/generator/SqliteAnalyzerPlugin.groovy
+++ analyzer/src/main/groovy/com/novoda/sqlite/generator/SqliteAnalyzerPlugin.groovy
@@ -2,13 +2,14 @@ package com.novoda.sqlite.generator
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.ProjectConfigurationException
 
 class SqliteAnalyzerPlugin implements Plugin<Project> {
     void apply(Project project) {
         ensurePluginDependencies(project)
         def extension = project.extensions.create('sqliteAccess', SqliteAnalyzerExtension, project)
         project.afterEvaluate {
-            project.android.applicationVariants.all { variant ->
+            project.android[variants(project)].all { variant ->
                 File sourceFolder = project.file("${project.buildDir}/generated/source/sqlite/${variant.dirName}")
                 def javaGenerationTask = project.tasks.create(name: "generate${variant.name.capitalize()}SqliteAccess", type: GenerateDBAccess) {
                     dbConnection extension.dbConnector
@@ -23,9 +24,26 @@ class SqliteAnalyzerPlugin implements Plugin<Project> {
     }
 
     private static void ensurePluginDependencies(Project project) {
-        def hasAndroid = project.plugins.hasPlugin('android') || project.plugins.hasPlugin('android-library')
-        if (!hasAndroid) {
-            throw new RuntimeException("The 'android' or 'android-library' plugin is required.")
+        if (!isApplication(project) && !isLibrary(project)) {
+            throw new ProjectConfigurationException("The 'android' or 'android-library' plugin is required.", null)
         }
     }
+
+    private static String variants(Project project) {
+        if (isApplication(project)) {
+            return 'applicationVariants'
+        } else if (isLibrary(project)) {
+            return 'libraryVariants'
+        }
+        throw new ProjectConfigurationException("The 'android' or 'android-library' plugin is required.", null)
+    }
+
+    private static boolean isLibrary(Project project) {
+        project.plugins.hasPlugin('com.android.library') || project.plugins.hasPlugin('android-library')
+    }
+
+    private static boolean isApplication(Project project) {
+        project.plugins.hasPlugin('com.android.application') || project.plugins.hasPlugin('android')
+    }
+
 }
