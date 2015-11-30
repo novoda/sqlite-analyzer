69e9e2c45242c62140239c09f473c51716097c2e
Volker Leck <volker.leck@gmail.com>
2015-11-30 16:29:08 +0100

    format JavaMapper with 4 char indentation

--- analyzer/src/main/groovy/com/novoda/sqlite/generator/JavaMapper.groovy
+++ analyzer/src/main/groovy/com/novoda/sqlite/generator/JavaMapper.groovy
@@ -2,34 +2,34 @@ package com.novoda.sqlite.generator
 
 class JavaMapper {
 
-  static String javaName(String name) {
-    def accessor = javaAccessor(name)
-    if (accessor.size() == 1)
-      return accessor[0].toLowerCase()
-    return accessor[0].toLowerCase() + accessor[1..-1]
-  }
-
-  static String javaAccessor(String name) {
-    assert name.size() > 0
-    return camelize(javaCompliant(name))
-  }
-
-  private static String camelize(String compliant) {
-    def builder = new StringBuilder()
-    compliant.split("_").each {
-      builder.append(it.toLowerCase().capitalize())
+    static String javaName(String name) {
+        def accessor = javaAccessor(name)
+        if (accessor.size() == 1)
+            return accessor[0].toLowerCase()
+        return accessor[0].toLowerCase() + accessor[1..-1]
     }
-    builder.toString()
-  }
 
-  private static String javaCompliant(String name) {
-    char[] chars = name.chars
-    char[] javaChars = new char[chars.length]
-    javaChars[0] = Character.isJavaIdentifierStart(chars[0]) ? chars[0] : '_'
-    for (int i = 1; i < javaChars.length; i++) {
-      javaChars[i] = Character.isJavaIdentifierPart(chars[i]) ? chars[i] : '_'
+    static String javaAccessor(String name) {
+        assert name.size() > 0
+        return camelize(javaCompliant(name))
     }
-    new String(javaChars)
-  }
+
+    private static String camelize(String compliant) {
+        def builder = new StringBuilder()
+        compliant.split("_").each {
+            builder.append(it.toLowerCase().capitalize())
+        }
+        builder.toString()
+    }
+
+    private static String javaCompliant(String name) {
+        char[] chars = name.chars
+        char[] javaChars = new char[chars.length]
+        javaChars[0] = Character.isJavaIdentifierStart(chars[0]) ? chars[0] : '_'
+        for (int i = 1; i < javaChars.length; i++) {
+            javaChars[i] = Character.isJavaIdentifierPart(chars[i]) ? chars[i] : '_'
+        }
+        new String(javaChars)
+    }
 }
 
